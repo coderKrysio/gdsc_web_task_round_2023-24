d77e9c9d1d17a9e2d932108098905c51f41a4704
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-16 18:34:41 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { Navbar } from '@/components/navbar';
 import { Camera } from 'lucide-react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-white text-black p-24">
-      gdsc task round
-      <Camera color="red" size={48} />
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center bg-white text-black p-24">
+			<Navbar />
+			gdsc task round
+			<Camera color="red" size={48} />
+		</main>
+	)
 }
