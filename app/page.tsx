c4fa016fa6e9f2ea39339bf3950bfb5c809add9e
Anushree <anushreejaiswal.srn@gmail.com>
2023-10-16 17:24:19 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Camera } from 'lucide-react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white text-black p-24">
+      gdsc task round
+      <Camera color="red" size={48} />
+    </main>
+  )
+}
